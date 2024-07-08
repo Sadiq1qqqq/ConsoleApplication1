de47ffc614998fdf055cc5edc894fa4ac1c4963a
sadiq <sadiq@SADIQ>
2024-07-09 02:41:12 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,92 @@
+﻿
+#include <iostream>
+
+using namespace std;
+//
+//int main()
+//{
+//    cout << "\tresume:" <<endl;
+//    cout << "name:Sadiq ";
+//    cout << "surname:Quliyev ";
+//    cout << "age:14 ";
+//    cout << "hobby:play computer ";
+//
+//    return 0;
+//}
+//
+//int main()
+//{
+//    cout << "\t---" <<endl;
+//    cout << "       /   \\" <<endl;
+//    cout << "      /     \\" << endl;
+//    cout << "      |     |" << endl;
+//    cout << "      |     |" << endl;
+//    cout << "      |     |" << endl;
+//    cout << "      \\     /" << endl;
+//    cout << "       \\   /" << endl;
+//    cout << "\t---" << endl;
+//    return 0;
+//}
+
+//int main()
+//{
+//	cout << "\"To be" <<endl;
+//	cout << "\tor not" << endl;
+//	cout << "\t\tto be!\"" << endl;
+//	cout << "\t\t\t/shakespeare/" << endl;
+//}
+//int main()
+//{
+//	cout << "     Escape sequences" <<endl;
+//	cout << "\n\\a\tbell (alert)" << endl;
+//	cout << "\n\\b\tbackspace" << endl;
+//	cout << "\n\\n\tnew line" << endl;
+//	cout << "\n\\r\tcarriage return" << endl;
+//	cout << "\n\\t\thorizontal tab" << endl;
+//	cout << "\n\\\\\tbackslash \\" << endl;
+//	cout << "\n\\\"\tdouble quotation mark" << endl;
+//	cout << "\n\\\'\tsingle  quotation mark" << endl;
+//}
+////{
+////	cout <<"\"   '\\t',   '\\n',   '\\\',   '\\'',   '\\'' ',  \"";
+////}
+//int main()
+//{
+//	cout << "        ********" << endl;
+//	cout << "       |                 *" << endl;
+//	cout << "      * |                * *" << endl;
+//	cout << "     *  |               *  *" << endl;
+//	cout << "    *   |              *   *" << endl;
+//	cout << "   *    |             *    *" << endl;
+//	cout << "  *     |            *     *" << endl;
+//	cout << " ********      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *      |           *      *" << endl;
+//	cout << " *     /------------------" << endl;
+//	cout << " *    /-------------*     *" << endl;
+//	cout << " *   /--------------*    *" << endl;
+//	cout << " *  /---------------*   *" << endl;
+//	cout << " * /----------------*  *" << endl;
+//	cout << " /----------------- *" << endl;
+//	cout << " *******" << endl;
+//}
+//int main()
+//{
+//	cout << "#include <iostream>"<<endl;
+//	cout << "using namespace std;" << endl;
+//	cout << "void main()" << endl;
+//	cout << "{" << endl;
+//	cout << "cout <<\" c++ forever!! \"; "<< endl;
+//	cout << "}" << endl;
+//
+//}
